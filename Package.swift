62f18eea7e411b5330d460af870b0dfcc409680c
Christopher Fuller <christopher.fuller@gotinder.com>
2023-09-02 11:39:03 -0700

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,55 @@
+// swift-tools-version:5.8
+
+import PackageDescription
+
+let packageName = "CollectionBuilders"
+
+enum SwiftLint {
+    static let plugin = "SwiftLintPlugin-\(packageName)"
+    static let binary = "SwiftLintBinary-\(packageName)"
+}
+
+let package = Package(
+    name: packageName,
+    products: [
+        .library(
+            name: "CollectionBuilders",
+            targets: ["CollectionBuilders"]),
+    ],
+    dependencies: [
+        .package(
+            url: "https://github.com/Quick/Nimble.git",
+            from: "12.0.0"),
+        .package(
+            url: "https://github.com/pointfreeco/swift-snapshot-testing.git",
+            from: "1.11.0"),
+    ],
+    targets: [
+        .target(
+            name: "CollectionBuilders",
+            plugins: [
+                .plugin(name: SwiftLint.plugin),
+            ]),
+        .testTarget(
+            name: "CollectionBuildersTests",
+            dependencies: [
+                "CollectionBuilders",
+                "Nimble",
+                .product(name: "SnapshotTesting", package: "swift-snapshot-testing"),
+            ],
+            plugins: [
+                .plugin(name: SwiftLint.plugin),
+            ]),
+        .plugin(
+            name: SwiftLint.plugin,
+            capability: .buildTool(),
+            dependencies: [
+                .target(name: SwiftLint.binary)
+            ],
+            path: "Plugins/SwiftLintPlugin"),
+        .binaryTarget(
+            name: SwiftLint.binary,
+            url: "https://github.com/realm/SwiftLint/releases/download/0.52.4/SwiftLintBinary-macos.artifactbundle.zip",
+            checksum: "8a8095e6235a07d00f34a9e500e7568b359f6f66a249f36d12cd846017a8c6f5"),
+    ]
+)
---
+++ README.md
@@ -0,0 +1,5 @@
+[![Swift](https://github.com/Tinder/CollectionBuilders/actions/workflows/swift.yml/badge.svg?event=push)](https://github.com/Tinder/CollectionBuilders/actions/workflows/swift.yml)
+
+# CollectionBuilders
+
+Swift result builders for Array and Set
